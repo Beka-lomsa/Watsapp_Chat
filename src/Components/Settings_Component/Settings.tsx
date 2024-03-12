b67a642353a32f7666737e2832967a9083958c05
Beka-Lomsa <beqa.lomsadze3@gmail.com>
2024-03-13 02:40:42 +0400

    init

--- src/Components/Settings_Component/Settings.tsx
+++ src/Components/Settings_Component/Settings.tsx
@@ -55,13 +55,24 @@ const Settings = ({
 
   return (
     <div
-      className={`h-[43.4rem] w-[15rem] bg-headerBg font-roboto text-maincommonColor
+      className={`w-[15rem] h-[43.4rem] bg-headerBg font-roboto text-maincommonColor
+      2xl:w-[14rem] 2xl:h-[39.5rem]
+      xl:w-[13rem] xl:h-[36rem] 
+      sm:w-[11rem] sm:h-[31.5rem] sm:flex sm:flex-col sm:text-center
+      Xsm:w-[9rem] Xsm:h-[28.5rem]
+      
+
     ${darkMode && "dark:bg-darkcommon text-whiteletters"}
     `}
     >
-      <header className="w-[100%] flex justify-end items-center  py-[1rem] px-[0.5rem]">
+      <header className="w-[100%] flex justify-end items-center  py-[0.7rem] px-[0.5rem]">
         <div className="m-auto">
-          <h1 className="text-[0.95rem] font-bold">
+          <h1
+            className="text-[0.95rem] font-bold
+          xl:text-sm 
+          Xsm:text-xs
+          "
+          >
             {language === "Georgian"
               ? "პარამეტრები"
               : language === "English"
@@ -76,13 +87,22 @@ const Settings = ({
             onClick={() => setSettingsVisible(false)}
             src={darkMode ? darkClose : closeX}
             alt="closeX"
-            className="h-[1.2rem] w-[1.2rem] cursor-pointer"
+            className="h-[1.2rem] w-[1.2rem] cursor-pointer
+            xl:w-[1.15rem] sm:h-[1.15rem]
+            Xsm:w-[1rem] Xsm:h-[1rem]
+            "
           />
         </div>
       </header>
-      <section className="flex justify-center">
+      <section
+        className="flex justify-center 
+         sm:text-center
+      "
+      >
         <div className="w-[12.5rem] flex flex-col">
-          <h4 className="mt-[3.5rem] text-[0.75rem]">
+          <h4 className="mt-[3.5rem] text-[0.75rem]
+          sm:my-[0.1rem]
+          ">
             {language === "Georgian"
               ? "მომხმარებლის სახელი"
               : language === "English"
@@ -93,6 +113,8 @@ const Settings = ({
           </h4>
           <input
             className={`h-[1.5rem] mt-[0.2rem] outline-none border border-frameColors
+            sm:w-[10rem] sm:m-auto
+            Xsm:w-[7rem]
              ${darkMode && "dark:bg-darkHeadInput border-0"}
           `}
             type="text"
@@ -100,7 +122,10 @@ const Settings = ({
 
           {/* Light & Dark / Modes */}
           <div
-            className="flex items-center text-center mt-[2.5rem]"
+            className="flex items-center text-center mt-[2.5rem]
+            sm:ml-[2.1rem] sm:mt-[1.5rem]
+            Xsm:ml-[2.2rem]
+            "
             typeof="dark_light"
           >
             <button
@@ -108,13 +133,18 @@ const Settings = ({
               className="h-[1.3rem] w-[2.5rem] bg-frameColors rounded-full flex items-center transition duration-300"
             >
               <div
-                className={`h-[1.3rem] w-[1.3rem] border border-frameColors bg-headerBg rounded-full transition duration-500 ${
+                className={`h-[1.3rem] w-[1.3rem] border border-frameColors bg-headerBg rounded-full transition duration-500 
+                
+                ${
                   darkMode &&
                   "translate-x-[1.2rem]  dark:bg-darkYellow border border-darkYellow"
                 }`}
               />
             </button>
-            <p className="text-[0.75rem] ml-[1rem] font-normal">
+            <p className="text-[0.75rem] ml-[1rem] font-normal
+            sm:ml-[1.5rem]
+            Xsm:ml-[1rem]
+            ">
               {language === "Georgian"
                 ? "ბნელი რეჟიმი"
                 : language === "English"
@@ -127,7 +157,10 @@ const Settings = ({
 
           {/* CTRL + ENTER Shortcut */}
           <div
-            className="flex items-center text-center mt-[0.8rem]"
+            className="flex items-center text-center mt-[0.8rem]
+            sm:justify-center
+            Xsm:justify-items-center Xsm:px-{rem}
+            "
             typeof="CTRL + ENTER Shortcut"
           >
             <button
@@ -141,20 +174,32 @@ const Settings = ({
               `}
               />
             </button>
-            <p className="text-[0.75rem] ml-[1rem] font-normal">
-              {language === "Georgian"
-                ? "გაგზავნა"
-                : language === "English"
-                ? "Send on"
-                : language === "Spanish"
-                ? "Enviar con"
-                : "Default Result"}
-              <span className="font-medium ml-[0.2rem]">CTRL+ENTER</span>
+            <p
+              className="text-[0.75rem] ml-[1rem] font-normal flex
+              sm:block
+              Xsm:flex Xsm:flex-col Xsm:ml-[1rem]
+            "
+            >
+              <div>
+                {language === "Georgian"
+                  ? "გაგზავნა"
+                  : language === "English"
+                  ? "Send on"
+                  : language === "Spanish"
+                  ? "Enviar con"
+                  : "Default Result"}
+              </div>
+              <span className="font-medium ml-[0.2rem]
+              Xsm:ml-[0rem]
+              ">CTRL+ENTER</span>
             </p>
           </div>
 
           {/* Time Format */}
-          <div className="flex flex-col mt-[2rem] text-[0.75rem]">
+          <div className="flex flex-col mt-[2rem] text-[0.75rem]
+          sm:mt-[1rem]
+          Xsm:ml-[0.3rem]
+          ">
             <p className="text-[0.75rem] mb-[0.3rem]">
               {language === "Georgian"
                 ? "დროის ფორმატი"
@@ -166,7 +211,11 @@ const Settings = ({
             </p>
 
             {/* 12HRS */}
-            <div className="flex w-[12.5rem] justify-start">
+            <div
+              className="flex w-[12.5rem] justify-start
+              sm:pl-[2rem]
+            "
+            >
               <div
                 typeof="12_HRS_Part"
                 className="flex items-center cursor-pointer"
@@ -181,11 +230,11 @@ const Settings = ({
                 </div>
                 <p className="text-[0.75rem] font-light ml-[0.5rem]">
                   {language === "Georgian"
-                    ? "12 საათი"
+                    ? "12 სთ"
                     : language === "English"
                     ? "12 hrs"
                     : language === "Spanish"
-                    ? "12 horas"
+                    ? "12 hrs"
                     : "Default Result"}
                 </p>
               </div>
@@ -193,7 +242,10 @@ const Settings = ({
               {/* 24-HRS */}
               <div
                 typeof="24_HRS_Part"
-                className="flex items-center cursor-pointer m-auto"
+                className="flex items-center cursor-pointer m-auto
+                sm:ml-[1rem]
+                Xsm:ml-[0.6rem]
+                "
               >
                 <div
                   onClick={() => setClockDisplay(false)}
@@ -205,21 +257,24 @@ const Settings = ({
                 </div>
                 <p className="text-[0.75rem] font-light ml-[0.5rem]">
                   {language === "Georgian"
-                    ? "24 საათი"
+                    ? "24 სთ"
                     : language === "English"
                     ? "24 hrs"
                     : language === "Spanish"
-                    ? "24 horas"
+                    ? "24 hrs"
                     : "Default Result"}
                 </p>
               </div>
             </div>
           </div>
           {/* Language */}
-          <div className="mt-[2rem]">
-            <p className="text-[0.75rem] mb-[0.3rem]">
+          <div className="mt-[2rem]
+          sm:mt-[1rem]
+          ">
+            <p className="text-[0.75rem] mb-[0.3rem]
+            ">
               {language === "Georgian"
-                ? "ენა"
+                ? "შეარჩიე ენა"
                 : language === "English"
                 ? "Language"
                 : language === "Spanish"
@@ -228,7 +283,11 @@ const Settings = ({
             </p>
             <button
               onClick={handleDropDownList}
-              className={`w-[100%] flex justify-between items-center px-[0.6rem] py-[0.3rem] border border-frameColors bg-yellow-100 ${
+              className={`w-[100%] flex justify-between items-center px-[0.6rem] py-[0.3rem] border border-frameColors bg-yellow-100
+              sm:w-[10rem] sm:m-auto sm:mt-[0.5rem]
+              Xsm:w-[7rem]
+              ${
+                
                 dropDown && "border-b-0"
               } 
                 ${darkMode && "dark:bg-darkHeadInput border-0"}
@@ -252,6 +311,8 @@ const Settings = ({
             {dropDown && (
               <div
                 className={`w-full text-[0.75rem] font-light text-downErrow cursor-pointer border border-frameColors
+                sm:w-[10rem] sm:m-auto
+                Xsm:w-[7rem]
                 ${
                   darkMode &&
                   "dark:border-0 dark:text-whiteletters dark:bg-neutral-800"
@@ -288,33 +349,39 @@ const Settings = ({
         </div>
       </section>
       {/* Reset Settings to default */}
-      <footer className="w-full flex justify-end absolute bottom-0">
+      <footer className="w-full flex justify-end absolute bottom-0 
+        sm:justify-center
+      ">
         <button
           onClick={handleResetSetting}
-          className="flex px-[0.5rem] py-[0.5rem] cursor-pointer hover:underline"
+          className="flex px-[0.5rem] py-[0.5rem] cursor-pointer hover:underline
+          sm:align-middle sm:items-center
+          "
         >
           {darkMode ? (
             <BsTrash3 className="mx-[0.7rem] w-[1rem] h-[0.95rem]" />
           ) : (
             <img
-              className="mx-[0.7rem] w-[1rem] h-[0.95rem]"
+              className="mx-[0.7rem] w-[1rem] h-[0.95rem]
+              sm:mx-[0rem]
+              "
               src={trash}
               alt="trash"
             />
           )}
           <p
             className={`text-[0.75rem] font-normal text-maincommonColor
+            sm:ml-[0.2rem]
               ${darkMode && "dark:text-whiteletters"}
             `}
           >
             {language === "Georgian"
-                ? "ნაგულისხმები პარამეტრები"
-                : language === "English"
-                ? "Reset Settings to default"
-                : language === "Spanish"
-                ? "Ajustes predeterminados"
-                : "Default Result"}
-            
+              ? "ნაგულისხმები პარამეტრები"
+              : language === "English"
+              ? "Reset Settings to default"
+              : language === "Spanish"
+              ? "Ajustes predeterminados"
+              : "Default Result"}
           </p>
         </button>
       </footer>
